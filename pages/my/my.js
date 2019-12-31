3fa867d4b597ac959e1d7e46099fd939da8c0947
Eternalroin <42405077+Eternalroin@users.noreply.github.com>
2019-12-31 10:31:49 +0800

    update

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,18 +1,22 @@
 // pages/my/my.js
+//获取应用实例
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+    userInfo:app.globalData.userInfo
+    })
   },
 
   /**
