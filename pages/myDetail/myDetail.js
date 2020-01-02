e70f6c69dc0ac2eb89d329d1cb0b79b6ae8c67ba
Eternalroin <42405077+Eternalroin@users.noreply.github.com>
2020-01-02 15:48:40 +0800

    update

--- pages/myDetail/myDetail.js
+++ pages/myDetail/myDetail.js
@@ -5,17 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    selectArray: [{
-      'id': '10',
-      'value': '会计类'
-    }, {
-      "id": "21",
-      "text": "工程类"
-    }, '技术类', {
-      'value': '其他'
-    }],
     select: false,
-    tihuoWay: '本日'
+    tihuoWay: '本月'
   },
 
   /**
